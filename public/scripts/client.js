9df026f66293c7f5aaf652da7a0689d3ea923cf5
calgarybino <binoandemeskel1234@gmail.com>
2023-08-02 22:32:41 -0400

    screenshot Image from app

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,53 +1,154 @@
-// Wait for the DOM to be loaded before executing any JavaScript
-document.addEventListener("DOMContentLoaded", function () {
-  // Get references to necessary DOM elements
-  const tweetForm = document.getElementById("tweet-form");
-  const tweetText = document.getElementById("tweet-text");
-  const errorContainer = document.getElementById("error-container");
-  const tweetsContainer = document.getElementById("tweets-container");
-  const scrollToTopBtn = document.querySelector(".scroll-to-top-btn");
+/*
+ * Client-side JS logic goes here
+ * jQuery is already loaded
+ * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+ */
 
-  // Function to handle the form submission
-  tweetForm.addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the form from submitting normally
+$(document).ready(function () {
+  // Hide error container on page load
+  $("#error-container").hide();
 
-    // Validate the tweet text
-    const tweetContent = tweetText.value.trim();
-    if (tweetContent === "") {
-      showError("Tweet text cannot be empty.");
-      return;
+  //function to render a single tweet element
+  const createTweetElement = function (tweet) {
+    const $tweet = $(`
+      <article class="tweet">
+        <header>
+          <div class="user-info">
+            <img src="${tweet.user.avatars}" alt="User Avatar">
+            <h3 class="username">${tweet.user.name}</h3>
+          </div>
+          <span class="handle">${tweet.user.handle}</span>
+        </header>
+        <div class="tweet-content">
+          <p>${escape(tweet.content.text)}</p>
+        </div>
+        <footer>
+          <span class="timeago">${timeago.format(tweet.created_at)}</span>
+          <div class="icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+          </div>
+        </footer>
+      </article>
+    `);
+
+    return $tweet;
+  };
+
+  // Function to escape user-generated content
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  //function to render tweets
+  const renderTweets = function (tweets) {
+    //clear the existing tweets container
+    $("#tweets-container").empty();
+
+    // Iterate over the tweets array and append each tweet to the container
+    tweets.forEach(function (tweet) {
+      const $tweetElement = createTweetElement(tweet);
+      $("#tweets-container").prepend($tweetElement);
+
+      // Update timestamp using timeago library
+      const $timestamp = $tweetElement.find(".timeago");
+      $timestamp.text(timeago.format(tweet.created_at));
+    });
+  };
+
+  // function to load tweets from the server
+  const loadTweets = function () {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      dataType: "json",
+      success: function (response) {
+        //call the renderTweets function with the response tweets
+        renderTweets(response);
+      },
+      error: function (error) {
+        //Handle the error response
+        console.log("Error loading tweets:", error);
+      },
+    });
+  };
+  // call the loadTweets function to load tweets on page load
+  loadTweets();
+
+  // Event listener for form submission
+  $("#tweet-form").submit(function (event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+
+    // Hide the error container before proceeding with validation
+    $("#error-container").slideUp();
+
+    // Get the form data and serialize it as a query string
+    const $form = $(this);
+    const $tweetText = $form.find("#tweet-text");
+    const tweetData = $form.serialize();
+
+    //validate the tweet content before sending form data to server
+    const tweetContent = $tweetText.val();
+    if (tweetContent.trim() === "") {
+      const errorMessage = "Tweet text cannot be empty.";
+      displayErrorMessage(errorMessage);
+      return; // prevent the empty tweet from being sent to server
     }
 
-    // If the tweet is valid, you can handle it here (e.g., send it to the server)
+    if (tweetContent.length > 140) {
+      // Display an error message for exceeding 140 characters
+      const errorMessage = "Tweet content exceeds the 140 character limit!";
+      displayErrorMessage(errorMessage);
+      return; // Prevent form submission if content exceeds the limit
+    }
 
-    // Clear the tweet text area
-    tweetText.value = "";
+    // Send the AJAX POST request
+    $.ajax({
+      url: "/tweets",
+      method: "POST",
+      data: tweetData,
+      success: function (response) {
+        // Handle the success response
+        console.log("Tweet submitted successfully:", response);
 
-    // Update the character counter to 140
-    const counter = document.querySelector(".counter");
-    counter.textContent = "140";
+        // Clear the textarea
+        $tweetText.val("");
 
-    // Clear any previous error messages
-    clearError();
+        // Reset the character counter to 140
+        $(".counter").text("140");
+
+        //Fetch and render all tweets from server including the new one
+        loadTweets();
+      },
+      error: function (error) {
+        // Handle the error response
+        console.log("Error submitting tweet:", error);
+      },
+    });
+    function displayErrorMessage(message) {
+      // Insert the error message into the error container
+      $("#error-container").html(
+        `<i class="fa-solid fa-exclamation-triangle"></i> ${message}`
+      );
+      // Show the error container with slide down animation
+      $("#error-container").slideDown();
+    }
   });
 
-  // Function to show an error message
-  function showError(message) {
-    errorContainer.textContent = message;
-    errorContainer.style.display = "block";
-  }
-
-  // Function to clear error messages
-  function clearError() {
-    errorContainer.textContent = "";
-    errorContainer.style.display = "none";
-  }
-
-  // Function to handle the scroll-to-top button
-  scrollToTopBtn.addEventListener("click", function () {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+  // Event listener for "Write" button click
+  $(".write-tweet-btn").click(function () {
+    // Toggle the visibility of the new-tweet section
+    $(".new-tweet").slideToggle("slow", function () {
+      // After the animation is complete, focus on the textarea
+      if ($(this).is(":visible")) {
+        $("#tweet-text").focus();
+      } else {
+        $(".header").css("margin-bottom", "20px"); // Add space to the header when the form is visible
+      }
+    });
   });
-
-  // You can add more JavaScript code here for other interactions and functionalities
-  
 });
